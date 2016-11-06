a77a51ae8a749f2a4ff722ee8369ab7fd401bc72
dmcgaa <mcgaa.david@gmail.com>
2016-11-06 19:55:17 +0000

    multer and XMLHttpRequest functional

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 var express = require('express');
 var app = express();
+// var bodyParser = require('body-parser');
+
+var multer = require('multer');
+var upload = multer();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/home', function(request, response) {
+  console.log("request received", request.url);
+  console.log('xhr?', request.xhr);
   response.render('pages/index')
+});
+
+app.post('/upload', function(req, res) {
+  console.log("request received", req.url);
+  console.log('xhr?', req.xhr);
+  // res.send(request.url);
+  console.log(req.body);
+  res.send("POST response on 'upload'");
+  // console.log("req:", req);
+  // res.send("file received");
+});
+
+// app.post('/somefile', upload.single('filemetadata'), function (req, res, next) {
+app.post('/somefile', upload.any(), function(req, res, next) {
+  console.log("POST to /somefile");
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log("files", req.files);
+  console.log("body", req.body)
+  console.log("req length", req.length);
+  // console.log(req.files[0]);
+  // res.send(req.files[0]);
+  res.send({
+    originalname: req.files[0].originalname,
+    size: req.files[0].size
+  });
+});
+
+// app.post('/somefile', upload.array(), function (req, res, next) {
+//   // req.body contains the text fields
+//   console.log(req.body);
+// });
+
+app.get('/upload', function(req, res) {
+  console.log(req.xhr);
+  res.send("GET response on 'upload'")
 });
 
 app.listen(app.get('port'), function() {
--- public/js/file-metadata.js
+++ public/js/file-metadata.js
@@ -1,16 +1,94 @@
 if (typeof document !== 'undefined') {
     var form = document.forms["filemetadata"];
-    document.getElementById("choose-file").addEventListener("click", chooseFileButtonClick);
-    form.addEventListener('submit', uploadButtonClick);
+    // document.getElementById("file-input").addEventListener("click", chooseFileButtonClick);
+    // form.addEventListener('submit', uploadButtonClick);
+    // document.getElementById("button-upload").addEventListener("click", formDataUpTest);
+    document.getElementById("button-upload").addEventListener("click", uploadButtonClick);
+    document.getElementById("change-content").addEventListener("click", loadDoc);
+    document.getElementById('file-input').addEventListener('change', chooseFileButtonClick, false);
+    var file;
 }
 
-function chooseFileButtonClick() {
-    alert("choose file clicked");
+function chooseFileButtonClick(e) {
+    // alert("choose file clicked");
+    file = e.target.files[0];
+    if (!file) {
+        console.log('no file');
+        return;
+    }
+    var reader = new FileReader();
+    reader.onloadend = () => {
+        console.log("reader finished", JSON.stringify(reader.result));
+    };
+    reader.onload = function(event) {
+        console.log("reader success", event.target.result);
+    };
+    // reader.readAsText(file);
+    form.append('upload', reader.readAsBinaryString(file));
+    // either attach the data back to the FormObject, or put the form data global
+}
+
+function formDataUpTest() {
+    // var formElement = document.querySelector("form");
+    var formData = new FormData();
+    var request = new XMLHttpRequest();
+    request.open("POST", "upload");
+    formData.append("serialnumber", 1234432);
+    console.log('sending', formData);
+    request.send(formData);
+}
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("POST", "upload", true);
+  xhttp.send();
 }
 
 function uploadButtonClick() {
-    alert("upload clicked");
-    // document.getElementById("filemetadata").submit();
+    // var form = document.forms["filemetadata"];
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    // xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    formData.append("CustomField", "This is some extra data");
+    formData.append('file', file);
+    console.log("Form Data Entries: ");
+    for(var pair of formData.entries()) {
+       console.log(pair[0]+ ', '+ pair[1]); 
+    }
+    console.log("readyState", xhr.readyState);
+    if(xhr.readyState == 0 || xhr.readyState == 4){
+        xhr.open("POST", 'somefile', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                console.log('status 200');
+                console.log(xhr.responseText);
+                var info = JSON.parse(xhr.responseText)
+                var alertStr = String.prototype.concat("Upload successful!\nFile: ", info.originalname, "\nSize: ", info.size, " bytes." )
+                alert(alertStr);
+            }
+        };
+        console.log("sending", formData);
+        xhr.send(formData);    
+    }
+    else {
+        setTimeout('process()', 1000);
+    }
+    // xhr.onload = function() {
+    //     if (xhr.status == 200) {
+    //         alert("choose file clicked");
+    //     }
+    //     else {
+    //         alert("Error " + xhr.status + " occurred when trying to upload your file.");
+    //     }
+    // };
+    
+    // xhr.send(null);
 }
 
 //nodeJS
